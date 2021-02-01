a73d5c4eb44015ea856ae57da39eb0088ea24a8f
JinseongHwang <eddy5360@naver.com>
2021-02-01 17:20:37 +0900

    feat: 위메프 기획전 크롤링 로직 구현 완료

---
+++ crawler/social_commerce/wemakeprice.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const URL = 'https://front.wemakeprice.com/promotions/main';
+
+const httpsPrefix = 'https:';
+const appendHttps = (url) => {
+  if (url.indexOf(httpsPrefix) === -1) {
+    // 접두사 'https:' 가 없는 URL일 경우
+    return httpsPrefix + url;
+  }
+  return url; // 정상적인 URL일 경우
+};
+
+const getTitle = (raw) => {
+  return raw.substr(raw.indexOf('_') + 1);
+};
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto(URL);
+
+  // 프로모션 페이지가 로딩될 때까지 대기
+  await page.waitForSelector('#_contents > div > div.promotion_list > ul');
+
+  const promotionList = await page.$$(
+    '#_contents > div > div.promotion_list > ul > li',
+  ); // 프로모션 리스트 추출: Promise
+
+  let scrappedData = [];
+
+  for (let promotion of promotionList) {
+    scrappedData.push({
+      index: await promotion.$eval('a[data-gtm-index]', (card) =>
+        card.getAttribute('data-gtm-index'),
+      ),
+      title: getTitle(
+        await promotion.$eval('a[data-gtm-label]', (card) =>
+          card.getAttribute('data-gtm-label'),
+        ),
+      ),
+      promotionUrl: appendHttps(
+        await promotion.$eval('a[href]', (card) => card.getAttribute('href')),
+      ),
+      imageUrl: await promotion.$eval('img[src]', (img) =>
+        img.getAttribute('src'),
+      ),
+    });
+  }
+
+  fs.writeFile(
+    `../data/wemakeprice-${new Date().getTime()}.json`,
+    JSON.stringify(scrappedData, null, 2),
+    (err) =>
+      err
+        ? console.error('[ERROR] Failed writing file.', err)
+        : console.log('Successfully file created.'),
+  );
+
+  await browser.close();
+})();
